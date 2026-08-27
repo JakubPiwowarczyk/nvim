b1a1ea8100cb8f56d36bdbb239a710e588d8d0dd
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2026-08-27 20:41:15 +0200

    add code completion and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      vim.lsp.config('lua_ls', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('gopls', {
+        capabilities = capabilities
+      })
+
       vim.lsp.enable('lua_ls')
       vim.lsp.enable('gopls')
     end
