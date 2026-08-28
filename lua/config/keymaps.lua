513587090e0837eb25868846aac656ff8adf297c
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2026-08-28 09:51:09 +0200

    add debugger

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
+
+vim.keymap.set("v", "\"", 'c"<C-r>""<Esc>', { noremap = false, silent = true, desc = "Encloses marked text in qoutes" })
+vim.keymap.set("n", "<leader>if", 'gg=G', { noremap = false, silent = true, desc = "Intend file" })
