63db921e2d5ae42ee45e844931f5bafd95324340
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2026-08-30 21:26:33 +0200

    move lsp related keymaps to /plugins/lsp-config.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,5 @@
 -- project / netrw
 vim.keymap.set("n", "<leader>E", "<cmd>Ex<CR>", { desc = "Open netrw" })
 
--- lsp
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
-vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
-
 vim.keymap.set("v", "\"", 'c"<C-r>""<Esc>', { noremap = false, silent = true, desc = "Encloses marked text in qoutes" })
 vim.keymap.set("n", "<leader>if", 'gg=G', { noremap = false, silent = true, desc = "Intend file" })
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,6 +31,14 @@ return {
 
       vim.lsp.enable('lua_ls')
       vim.lsp.enable('gopls')
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
+
     end
   }
 }
