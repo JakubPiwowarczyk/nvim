7ae9bde89623d0e576e261cddc8bbb984c4f56a7
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2026-09-09 20:24:10 +0200

    update lsp related keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,12 +32,12 @@ return {
       vim.lsp.enable('lua_ls')
       vim.lsp.enable('gopls')
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
     end
   }
